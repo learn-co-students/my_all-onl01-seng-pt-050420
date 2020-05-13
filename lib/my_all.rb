6a3d5e01b1fb57e03bbf6b748f80deae85a085e3
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 18:22:36 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,12 +6,15 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
   while i < collection.length
+  
   # declare our array & yield each element in the collection
+  
     block_return_values << yield(collection[i])
     i = i + 1 
   end
   
   # add an ' #include? ' method to determine the return value of the my_all? method.
+  
   if block_return_values.include?(false)
     false 
   else 

7146f7c2ad8f3d6d3a91c6148ea2ddd9c114129b
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 18:21:59 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
-def my_all?(collection)
+# Iterate through each element in the passed-in collection using the simple lower-level iterator 'while'. 
 
+def my_all?(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length
+  # declare our array & yield each element in the collection
+    block_return_values << yield(collection[i])
+    i = i + 1 
+  end
+  
+  # add an ' #include? ' method to determine the return value of the my_all? method.
+  if block_return_values.include?(false)
+    false 
+  else 
+    true 
+  end
 end
